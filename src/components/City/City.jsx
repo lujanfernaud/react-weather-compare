a9df76b1655a6cd12ff40b9afe041600710c22b3
lujanfernaud <lujanfernaud@gmail.com>
2018-09-09 19:14:58 +0100

    Remove card loader markup and styles

--- src/components/City/City.jsx
+++ src/components/City/City.jsx
@@ -6,22 +6,7 @@ class City extends Component {
   render() {
     return (
       <div className='information-card'>
-        <div className='information-card__loader'>
-          <h2 className='information-card__heading is-size-4'>
-          </h2>
-
-          <p className='information-card__status'>
-            Fetching data...
-          </p>
-
-          <img src='dist/svg/wi-thermometer.svg'
-            className='information-card__icon' />
-
-          <p className='information-card__temperature'>
-          </p>
-        </div>
-
-        <div className='information-card__data opacity-1'>
+        <div className='information-card__data'>
           <h2 className='information-card__heading is-size-4'>
             {this.props.city.name}
           </h2>
